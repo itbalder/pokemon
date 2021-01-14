4bbc6ff632c582b569edc954dc94d0cdd5fb279d
itbalder <ita.bal18@gmail.com>
2021-01-14 10:48:02 -0600

    Initial commit

---
+++ src/test/java/com/masters/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masters.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
