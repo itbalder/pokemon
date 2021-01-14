39acb1ae18d3b8d289a2862ad0547bc99e9d9877
itbalder <ita.bal18@gmail.com>
2021-01-14 13:06:39 -0600

    cambio en clases
    italia

--- src/main/java/com/masters/pokemon/Tipo.java
+++ src/main/java/com/masters/pokemon/Tipo.java
@@ -1,4 +1,12 @@
 package com.masters.pokemon;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Tipo {
+
+    @Id
+    @GeneratedValue
 }
--- src/main/java/com/masters/pokemon/TipoPokemon.java
+++ src/main/java/com/masters/pokemon/TipoPokemon.java
@@ -1,68 +1,33 @@
 package com.masters.pokemon;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+
+import javax.persistence.*;
 
 @Entity
 public class TipoPokemon {
+    public id getPokemon() {
+        return Pokemon;
+    }
+
+    public void setPokemon(id pokemon) {
+        Pokemon = pokemon;
+    }
+
+    public id getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(id tipo) {
+        Tipo = tipo;
+    }
+
+    @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private id Pokemon;
+
    @Id
-   @GeneratedValue (strategy = GenerationType.IDENTITY)
-   private 
+   @Column
+   private id Tipo;
+
 }
 
-
-
-    @Column
-    private String nombre;
-
-    @Column
-    private String apellido;
-
-    @Column
-    private String direccion;
-
-    @Column
-    private String telefono;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-}
