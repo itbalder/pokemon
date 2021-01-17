f007ab04217146113e3751fd4fe5fc4481873e77
itbalder <ita.bal18@gmail.com>
2021-01-17 11:12:22 -0600

    cambio

--- src/main/java/com/masters/pokemon/web/controller/PokemonDataEntityController.java
+++ src/main/java/com/masters/pokemon/web/controller/PokemonDataEntityController.java
@@ -11,10 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import java.security.Policy;
 import java.util.List;
 import java.util.Optional;
 
-//Indica que s spring que esta clase sera un controlador de un api rest
+//This is a class and will be a controller for the api rest
 @RestController
 @RequestMapping("/pokemon")
 public class PokemonDataEntityController {
@@ -26,21 +28,20 @@ public class PokemonDataEntityController {
     @ApiOperation("Search every pokemon on the database")
     @ApiResponse(code = 200, message = "Ok")
     public ResponseEntity<List<PokemonData>> getAll() {
-        return new ResponseEntity<>(pokemonDataService.getAll(),HttpStatus.OK);
+        return new ResponseEntity(pokemonDataService.getAll(),HttpStatus.OK);
     }
 
 
-
     @GetMapping("/findById/{dataId}")
-    @ApiOperation("Search a pokemon with an ID")
+    @ApiOperation("Search a pokemon with a ID")
     @ApiResponses({
             @ApiResponse(code = 200, message = "OK"),
             @ApiResponse(code = 404, message = "Product Not found")
     })
-    public ResponseEntity<PokemonData> getokemon(@ApiParam(value = "The id of a pokemon", required = true, example = "2")@PathVariable("dataId")int idDataPokemon) {
+    public ResponseEntity<PokemonData> getPokemon(@ApiParam(value = "The id of a the pokemon is:", required = true, example = "2")@PathVariable("dataId")int idDataPokemon) {
         return pokemonDataService.getPokemon(idDataPokemon)
-                .map(pokemonData -> new ResponseEntity<>(pokemonData,HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+                .map(pokemonData -> new ResponseEntity(pokemonData,HttpStatus.OK))
+                .orElse(new ResponseEntity(HttpStatus.NOT_FOUND));
     }
 
 
@@ -49,11 +50,11 @@ public class PokemonDataEntityController {
     @ApiResponse(code = 200, message = "Ok")
     public ResponseEntity<PokemonData> save(@RequestBody PokemonData pokemonData) {
 
-        return new ResponseEntity<>( pokemonDataService.save(pokemonData),HttpStatus.CREATED);
+        return new ResponseEntity( pokemonDataService.save(pokemonData),HttpStatus.CREATED);
     }
 
 
-    @DeleteMapping("/delete/{dataId}")
+    @DeleteMapping("/delete/{dataid}")
     public ResponseEntity delete(@PathVariable("dataId")int idDataPokemon) {
         if(pokemonDataService.delete(idDataPokemon)){
             return new ResponseEntity(HttpStatus.OK);
